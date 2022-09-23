e6081995ec63f4ab59425a127f8b4bbccd90079c
Gabriel <62155954+GABE400@users.noreply.github.com>
2022-09-24 00:41:49 +0200

    react project

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -6,12 +6,16 @@ import ThreePng from '../../images/img/3.png';
 import FourPng from '../../images/img/04.png';
 import FivePng from '../../images/img/05.png';
 import SixPng from '../../images/img/06.png';
+import SevenPng from '../../images/img/07.png';
+import EightPng from '../../images/img/08.png';
+import NinePng from '../../images/img/09.png';
 import { Link } from 'react-scroll';
 
 const Experience = () => {
   return (
     <section className='experience' id='experience'>
       <h1>Skills</h1>
+      <h2>Programming Languages</h2>
       <div className='services-container'>
         <div className='imgTech'>
           <img src={OnePng} alt='css' />
@@ -30,6 +34,15 @@ const Experience = () => {
         </div>
         <div className='imgTech'>
           <img src={SixPng} alt='Android' />
+        </div>
+        <div className='imgTech'>
+          <img src={SevenPng} alt='Android' />
+        </div>
+        <div className='imgTech'>
+          <img src={EightPng} alt='Android' />
+        </div>
+        <div className='imgTech'>
+          <img src={NinePng} alt='Android' />
         </div>
       </div>
 
