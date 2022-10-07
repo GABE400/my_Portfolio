a0250eb8b5e611b197da2f3b30f8cf58622a2fa9
Gabriel <62155954+GABE400@users.noreply.github.com>
2022-10-07 18:34:41 +0200

    react first commit

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -17,15 +17,14 @@ const About = () => {
         </h5>
         <p>
           Thank you for visiting my portfolio. I am a full stack developer, my
-          name is Gabriel Chipaya. I will brief you on what made me to choose
-          this career path as a developer. Well, basically the love of being in
-          a creative space, and the ability to come up with your own ideas, and
-          working towards the actual ideas is what made me to go for it.
-          However, i am into design especially web design, i am open to work
-          with UX/UI designs and graphic designs too. But my main interest is
-          being a full stack developer, I want to be flexible with the frontend
-          and backend. Solving problems with technology is my goal and I have a
-          passion to learn and to get better in the tech industry.
+          name is Gabriel Chipaya. My expertise is Web App and Mobile App
+          development, and i am open to work with UX/UI designs, Web designs and
+          graphic designs. I am flexible with the frontend and backend, my goal
+          is solving problems with technology and I have a passion to learn and
+          I am a team player. Let me brief you on what made me to choose this
+          career path as a developer. I have always been a creative person, I
+          loved being in a creative space, I have always wanted to come up with
+          my own ideas to turn my imagination to reality.
         </p>
         <Link
           className='button'
