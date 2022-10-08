d5c22252e1ec66ff85101b1deaeab654a35dd8b9
Gabriel <62155954+GABE400@users.noreply.github.com>
2022-10-09 00:36:49 +0200

    react is back

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -16,15 +16,14 @@ const About = () => {
           Fullstack <span>Developer</span>
         </h5>
         <p>
-          Thank you for visiting my portfolio. I am a full stack developer, my
-          name is Gabriel Chipaya. My expertise is Web App and Mobile App
-          development, and i am open to work with UX/UI designs, Web designs and
-          graphic designs. I am flexible with the frontend and backend, my goal
-          is solving problems with technology and I have a passion to learn and
-          I am a team player. Let me brief you on what made me to choose this
-          career path as a developer. I have always been a creative person, I
-          loved being in a creative space, I have always wanted to come up with
-          my own ideas to turn my imagination to reality.
+          Thank you for visiting my portfolio. I am a software developer, my
+          name is Gabriel Chipaya. My expertise is designing and developing
+          Desktop App, Web App, and Mobile App. I am flexible with the frontend
+          and backend, my goal is solving problems with technology and I have a
+          passion to learn, I am a team player. Let me brief you on what made me
+          to choose this career path as a developer. I have always been a
+          creative person, I loved being in a creative space, I have always
+          wanted to come up with my own ideas to turn my imagination to reality.
         </p>
         <Link
           className='button'
